382eff6b339d09384ddf1a153c1f5c53bd92139a
Markus.Svedberg <markussv.747@gmail.com>
2020-02-20 15:42:54 +0100

    inout

---
+++ hashcodetest/IO/Filereader.java
@@ -0,0 +1,49 @@
+package IO;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Filereader {
+
+    public static final String OS = System.getProperty("os.name").toLowerCase();
+
+    public static File readFile(String relPath) {
+        String path = "";
+
+        if(isMac()){
+            path = "./" + relPath;
+        }else if(isLin()){
+            path = "../" + relPath;
+        }else if(isWin()){
+            System.out.println("WHY USE THIS OS BRO, CLASSIC MELKER, brainshake?");
+            StringBuilder sb = new StringBuilder();
+            for (char c : relPath.toCharArray()) {
+                if(c != 47){
+                    sb.append(c);
+                }else {
+                    sb.append("\\");
+                }
+            }
+            path = "..\\" + relPath;
+        }else{
+            throw new IllegalArgumentException("BAD OS");
+        }
+
+        return new File(path);
+    }
+
+
+    private static  boolean isMac(){
+        return OS.startsWith("mac");
+    }
+
+    private static  boolean isWin(){
+        return OS.startsWith("win");
+    }
+    private static  boolean isLin(){
+        return OS.startsWith("lin");
+    }
+
+
+
+}
