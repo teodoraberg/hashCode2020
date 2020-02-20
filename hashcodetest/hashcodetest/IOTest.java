0dc4347ee8305e0d5f9d4a21abc3764382931a82
Teodor Åberg <teodor.aberg@gmail.com>
2020-02-20 16:03:16 +0100

    IOTest

---
+++ hashcodetest/hashcodetest/IOTest.java
@@ -0,0 +1,22 @@
+package hashcodetest;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import IO.*;  
+
+public class IOTest {
+    public static void main(String[] args) throws IOException {
+        File writeTo = null; 
+        try {
+            writeTo = IO.FileIO.getFile("out/1.txt"); 
+        }catch(Exception e) {
+            System.out.println("LUL");
+        }
+        FileWriter writer = new FileWriter(writeTo); 
+        
+        writer.append('a'); 
+        writer.close(); 
+        System.out.println("Done!");
+    }
+}
