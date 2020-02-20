9cdd6d7a1ad1dd2e795338e5afb783bdc850bf63
Teodor Åberg <teodor.aberg@gmail.com>
2020-02-20 21:11:14 +0100

    start alg

--- hashcodetest/teo/Solver.java
+++ hashcodetest/teo/Solver.java
@@ -1,6 +1,8 @@
 package teo;
 
 import java.io.File;
+import java.util.ArrayList;
+
 import IO.*; 
 
 public class Solver {
@@ -9,5 +11,12 @@ public class Solver {
     public void Solve(File file) {
         Inparser parser = new Inparser(file); 
         
+        ArrayList<Book> sortedLib = new ArrayList<Book>(); 
+        
+        
+        
+        
     }
+    
+    
 }
