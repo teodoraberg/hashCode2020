df7cb22c645312b7ae03938987f695205176b602
Markus.Svedberg <markussv.747@gmail.com>
2020-02-20 18:38:55 +0100

    döpa om tester

---
+++ hashcodetest/HC2020/ScoreTest.java
@@ -0,0 +1,13 @@
+package HC2020;
+
+import IO.FileIO;
+
+import java.io.File;
+
+public class ScoreTest {
+
+    public static void main(String[] args){
+        File testFile = FileIO.getFile("in/in1");
+    }
+
+}
