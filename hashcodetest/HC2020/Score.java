dc936354eeac9de41c140047d902c3431eb55536
Markus.Svedberg <markussv.747@gmail.com>
2020-02-20 19:13:57 +0100

    döpa om tester

--- hashcodetest/HC2020/Score.java
+++ hashcodetest/HC2020/Score.java
@@ -1,11 +1,36 @@
 package HC2020;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Score {
 
     public static int getScore(File ans){
+        HashSet<Integer> scoredBooks = new HashSet<>();
+
+        Scanner scan;
+        try{
+            scan = new Scanner(ans);
+        }catch (Exception e){
+            throw new IllegalArgumentException("DÅLIG FIL SKICKAD TILL SCORE");
+        }
+
+        if(scan.hasNext() == false){
+            System.out.println("TOM FIL TILL SCORE!");
+            return 0;
+        }
+
+        int numlibraries = Integer.parseInt(scan.next());
+        char[] books;
+        for(int i = 0; i < numlibraries; i++){
+            scan.nextLine();
+            books = scan.nextLine().trim().toCharArray();
+            for (char c : books) {
+                String s = String.valueOf(c);
+                scoredBooks.add(Integer.parseInt(s));
+            }
+        }
         return 0;
     }
-
 }
